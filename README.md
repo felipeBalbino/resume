89102160110a266d65dfda9e5ebd52711bbff8ae
Cezar Carneiro <cezargcarneiro@gmail.com>
2017-06-26 22:27:56 -0300

    small test with materialize

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Résumé
 This is my HTML/CSS Résumé.
-It is rendered with React and it hosted under GitHub Pages.
+It is rendered with React and hosted under GitHub Pages.
 
 You can see it live [here](https://cezar-carneiro.github.io/resume/).
 
--- src/Education/Education.js
+++ src/Education/Education.js
@@ -12,9 +12,24 @@ class Education extends Component {
     ));
 
     return (
-        <div id="education">
-            <h2>Education</h2>
-            {degrees}
+        // <div id="education">
+        //     <h2>Education</h2>
+        //     {degrees}
+        // </div>
+        <div className="row">
+            <div className="col s12 m6">
+            <div className="card">
+                <div className="card-content">
+                <span className="card-title">Card Title</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div className="card-action">
+                <a href="#">This is a link</a>
+                <a href="#">This is a link</a>
+                </div>
+            </div>
+            </div>
         </div>
     );
   }
